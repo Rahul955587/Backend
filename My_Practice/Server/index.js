e56f1111f87d575d893d2d17eeb1df2f50d6dbc8
Rahul <rahul955587@gmail.com>
2026-02-10 09:17:37 +0530

    server

--- My_Practice/Server/index.js
+++ My_Practice/Server/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const myserver = http.createServer((req, res) => {
     console.log("New Request received" );
-    res.end("Hello from serve");
+    res.end("<h1>Good Morning</h1>");
 });
 
 myserver.listen(8000, () => {

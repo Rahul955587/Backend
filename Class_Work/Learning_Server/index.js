9e9599372bc1f436ee3cbeebe417eecc0f6916f7
Rahul <rahul955587@gmail.com>
2026-02-11 12:44:45 +0530

    Server

---
+++ Class_Work/Learning_Server/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('hi');
+    const url = req.url;
+    const method = req.method;
+    // console.log(url, method);
+    if (url =="/" && method =="GET") {
+        res.end("Abhay123");
+    }
+    else if (url == "/home" && method == "GET") {
+        res.end("<h1>Hello</h1>")
+    }
+});
+
+server.listen(3000, () => {"successfully run" });
